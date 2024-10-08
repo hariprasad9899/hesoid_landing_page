d20b59be9dff010ef21a193854479eb4ebe3acb2
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2024-10-08 07:47:08 +0530

    dev completed hero section update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,16 +7,17 @@ const HeroSection = () => {
     return (
         <div className="flex flex-col items-center mt-6 lg:mt-20">
             <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-                VirtualR build tools
+                Dynamic Teams,
                 <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
                     {" "}
-                    for developers
+                    Consistent Success
                 </span>
             </h1>
             <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-                Empower your creativity and bring your VR app ideas to life with
-                our intuitive development tools. Get started today and turn your
-                imagination into immersive reality!
+                Our decentralized and flexible team delivers secure, scalable
+                solutions tailored to your needs. From web development to AI/ML
+                and cybersecurity, we provide innovative services that drive
+                lasting success for your business.
             </p>
             <div className="flex justify-center my-10">
                 <a
