1b3efd67bd1bb131ae7cd02f4e6b0c87adfa48f7
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2024-10-08 23:30:21 +0530

    dev removed additional expertise code

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -4,12 +4,6 @@ const Expertise = () => {
     return (
         <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
             <div className="text-center">
-                <span
-                    className="bg-neutral-900 text-blue-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase"
-                    id="expertise"
-                >
-                    Expertise
-                </span>
                 <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
                     Our{" "}
                     <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">
