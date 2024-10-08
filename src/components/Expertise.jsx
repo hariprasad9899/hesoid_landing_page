a4191dd865f6bbf3d3926ea792c89c043e2f1eb4
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2024-10-08 23:52:08 +0530

    dev fixed anchor tab issue

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -4,7 +4,10 @@ const Expertise = () => {
     return (
         <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
             <div className="text-center">
-                <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
+                <h2
+                    className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide"
+                    id="expertise"
+                >
                     Our{" "}
                     <span className="bg-gradient-to-r from-blue-500 to-blue-800 text-transparent bg-clip-text">
                         Expertise
