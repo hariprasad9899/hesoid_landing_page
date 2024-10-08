f03e9c0205da204879d75c04416d4834327e7334
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2024-10-08 07:27:40 +0530

    dev added new video in hero page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import video1 from "../assets/video1.mp4"
 import video2 from "../assets/video2.mp4"
+import video3 from "../assets/video3.mp4"
 
 const HeroSection = () => {
     return (
@@ -43,7 +44,7 @@ const HeroSection = () => {
                     muted
                     className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
                 >
-                    <source src={video2} type="video/mp4" />
+                    <source src={video3} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
